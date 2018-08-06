6760917f9044d831b9912e16782a37cd4af67e68
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-06 14:30:02 -0700

    cats api

--- js/catsApi.js
+++ js/catsApi.js
@@ -1,2 +1,66 @@
+const cats = [
+    {
+        name: 'Ball Cat',
+        url: '../../media/ball-cat.jpg'
+    },
+    {
+        name: 'Computer Cat',
+        url: '../../media/computer-cat.jpg'
+    },
+    {
+        name: 'Field Cat',
+        url: '../../media/field-cat.jpg'
+    },
+    {
+        name: 'Floppy Cat',
+        url: '../../media/floppy-cat.jpg'
+    },
+    {
+        name: 'Hands Up Cat',
+        url: '../../media/handsup-cat.jpg'
+    },
+    {
+        name: 'Head Stand Cat',
+        url: '../../media/headstand-cat.jpg'
+    },
+    {
+        name: 'Jumping Cat',
+        url: '../../media/jumping-cat.jpg'
+    },
+    {
+        name: 'Living Room Cat',
+        url: '../../media/livingroom-cat.jpg'
+    },
+    {
+        name: 'Pants Cat',
+        url: '../../media/pants-cat.jpg'
+    },
+    {
+        name: 'Pet Cat',
+        url: '../../media/pet-cat.jpg'
+    },
+    {
+        name: 'Pirate Cat',
+        url: '../../media/pirate-cat.jpg'
+    },
+    {
+        name: 'Pocket Cat',
+        url: '../../media/pocket-cat.jpg'
+    },
+    {
+        name: 'Sad Cat',
+        url: '../../media/sad-cat.jpg'
+    },
+    {
+        name: 'Screaming Cat',
+        url: '../../media/screaming-cat.jpg'
+    },
+    {
+        name: 'Winking Cat',
+        url: '../../media/winking-cat.jpg'
+    }
+];
 
-
+export default {
+    get: () => cats
+}
