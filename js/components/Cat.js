20320b34d60940a479a9678ab2c0a4338b5da547
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-06 14:54:54 -0700

    rendering a single cat

--- js/components/Cat.js
+++ js/components/Cat.js
@@ -1,14 +1,21 @@
 import html from '../html.js';
 
-const template = () => {
-  return html`
-      <li>This is a cat.</li>
-  `;
+const template = (cat) => {
+    return html`
+        <div>
+            <li>This is a ${cat.name}.</li>
+            <div class="cat" style="background: center / cover no-repeat url(${cat.url})"></div>
+        </div>
+    `;
 };
 
-export default class Product {
-  render() {
-      const dom = template();
-      return dom;
-  }
+export default class Cat {
+    constructor(props) {
+        this.cat = props.cat
+    }
+
+    render() {
+        const dom = template(this.cat);
+        return dom;
+    }
 };
