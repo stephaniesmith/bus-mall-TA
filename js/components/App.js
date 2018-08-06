fac22cb24458f2f17c47f35e6520f413a1b268bd
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-06 13:17:18 -0700

    rendered the poll to the app

--- js/components/App.js
+++ js/components/App.js
@@ -1,14 +1,23 @@
 import html from '../html.js';
+import Poll from './Poll.js';
 
 const template = () => {
     return html`
-        <h1>Hello World</h1>
+        <div>
+            <h1>Hello World</h1>
+        </div>
     `;
 };
 
 export default class App {
     render() {
         const dom = template();
+        this.div = dom.querySelector('div');
+        console.log(this.div);
+        const poll = new Poll();
+        console.log(poll);
+
+        this.div.appendChild(poll.render());
         return dom;
     }
 };
