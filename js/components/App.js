66d4a20cb5b0776f5a0b12757a702f8d9ef46d51
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-06 13:46:58 -0700

    removed console logs

--- js/components/App.js
+++ js/components/App.js
@@ -13,10 +13,10 @@ export default class App {
     render() {
         const dom = template();
         this.div = dom.querySelector('div');
-        console.log(this.div);
-        const poll = new Poll();
-        console.log(poll);
 
+        const poll = new Poll();
+
+        
         this.div.appendChild(poll.render());
         return dom;
     }
