6c4a53f794de1df5b04ccbfb8c8eeead791e083b
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-06 22:47:58 -0700

    created results component

--- js/components/Results.js
+++ js/components/Results.js
@@ -1,15 +1,19 @@
 import html from '../html.js';
 
-const template = () => {
+const template = (name, votes) => {
     return html`
-        <div>RESULTS!!!</div>
+        <li class="results"><strong>${name}</strong>: ${votes}</li>
     `;
 };
 
 export default class Results {
+    constructor(props) {
+        this.name = props.name;
+        this.votes = props.votes;
+    }
 
     render() {
-        const dom = template(this.cat);
+        const dom = template(this.name, this.votes);
         return dom;
     }
 }
