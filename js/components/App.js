8d52b85ceebb95ac48be14721cd591e1ef18ad25
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-08 10:49:23 -0700

    moved logic from the poll into the app

--- js/components/App.js
+++ js/components/App.js
@@ -1,37 +1,66 @@
 import html from '../html.js';
 import Poll from './Poll.js';
-import catApi from '../catsApi.js';
+import catsApi from '../catsApi.js';
+import Results from './Results.js';
+import ResultsChart from './ResultsChart.js';
 
 const template = () => {
     return html`
-        <div>
             <h1>Cute Cat Vote</h1>
-        </div>
+            <main>
+                <section>
+                    <div class="chart"></div>
+                    <ul class="results"></ul>
+                </section>
+            </main>
     `;
 };
 
 export default class App {
     constructor() {
-        this.cats = catApi.getRandomCats();
+        this.cats = catsApi.getRandomCats();
         this.rounds = 25;
+    }
+
+    renderResults(cats) {
+
+        this.rounds = 25;
+
+        cats.forEach(cat => {
+            const results = new Results({
+                url: cat.url,
+                views: cat.views,
+                votes: cat.votes
+            });
+            this.resultsUl.appendChild(results.render());
+        });
+
+        const resultsChart = new ResultsChart({
+            cats: cats
+        });
+
+        this.chartDiv.appendChild(resultsChart.render());
     }
 
     render() {
         const dom = template(this.rounds);
         this.div = dom.querySelector('div');
+        this.chartDiv = dom.querySelector('.chart');
+        this.resultsUl = dom.querySelector('.results');
 
         const poll = new Poll({
             cats: this.cats,
             rounds: this.rounds,
-            handleRounds: event => {
-                event.preventDefault();
+            handleRounds: (cat) => {
 
                 poll.rounds --;
-                this. rounds --;
+                this.rounds --;
 
-                // this.span.innerText = this.rounds;
+                catsApi.addVote(cat);
 
-                poll.tallyRounds(event);
+                poll.tallyRounds();
+
+                if(!this.rounds) this.renderResults(catsApi.get());
             }
         });
         
--- js/components/Poll.js
+++ js/components/Poll.js
@@ -1,14 +1,11 @@
 import html from '../html.js';
 import Cat from './Cat.js';
 import catsApi from '../catsApi.js';
-import Results from './Results.js';
-import ResultsChart from './ResultsChart.js';
 
 const template = (rounds) => {
     return html`
         <h2>Click on your favorite cat to vote.</h2>
         <h3>Total Rounds: <span>${rounds}</span></h3>
-        <div class="results"></div>
         <ul></ul>
     `;
 };
@@ -20,10 +17,7 @@ export default class Poll {
         this.handleRounds = props.handleRounds;
     }
 
-    tallyRounds(event) {
-        
-        catsApi.addVote(event.target.title);
-        
+    tallyRounds() {   
         this.span.innerText = this.rounds;
 
         while(this.ul.children.length) {
@@ -33,27 +27,6 @@ export default class Poll {
         if(this.rounds) { 
             this.cats = catsApi.getRandomCats();
             this.renderCats(this.cats);  
-        }
-        else {
-            this.rounds = 5;
-            this.ul.removeEventListener('click', this.handleRounds);
-
-            const cats = catsApi.get();
-
-            cats.forEach(cat => {
-                const results = new Results({
-                    url: cat.url,
-                    views: cat.views,
-                    votes: cat.votes
-                });
-                this.ul.appendChild(results.render());
-            });
-
-            const resultsChart = new ResultsChart({
-                cats: cats
-            });
-
-            this.resultsDiv.appendChild(resultsChart.render());
         }
     }
 
@@ -70,7 +43,6 @@ export default class Poll {
     render() {
         const dom = template(this.rounds);
         this.span = dom.querySelector('span');
-        this.resultsDiv = dom.querySelector('div.results');
         this.ul = dom.querySelector('ul');
 
         this.renderCats(this.cats);
