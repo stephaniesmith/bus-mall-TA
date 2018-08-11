e1f3952523bda19415bebbd8888ce3a754196dbd
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-11 08:29:04 -0700

    getting all the cats to show up on the cats page

--- js/components/AppProducts.js
+++ js/components/AppProducts.js
@@ -1,5 +1,7 @@
 import html from '../html.js';
+import catsApi from '../catsApi.js';
 import Header from './Header.js';
+import Cats from './Cats.js';
 import Footer from './Footer.js';
 
 let template = function() {
@@ -12,18 +14,28 @@ let template = function() {
 
 export default class AppProducts {
     constructor() {
-       
+        this.cats = catsApi.get();
     }
 
     render() {
         let dom = template();
         this.header = dom.querySelector('header');
+        this.main = dom.querySelector('main');
         this.footer = dom.querySelector('footer');
 
         const header = new Header();
+
+        const cats = new Cats({
+            cats: this.cats,
+            handleRounds: (cat) => {
+                console.log(cat);
+            }
+        });
+
         const footer = new Footer();
 
         this.header.appendChild(header.render());
+        this.main.appendChild(cats.render());
         this.footer.appendChild(footer.render());
 
         return dom;
