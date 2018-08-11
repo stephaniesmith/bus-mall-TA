4fbbbd2c03fc3ecc1ea3369ff882327b44d7b96e
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-11 13:17:49 -0700

    setting up local storage

---
+++ js/services/data.js
@@ -0,0 +1,21 @@
+const storedCats = window.localStorage.getItem('cats');
+
+let cats;
+
+storedCats && storedCats !== 'undefined' 
+    ? cats = JSON.parse(storedCats)
+    : cats = {};
+
+export default cats;
+
+window.addEventListener('beforeunload', () => {
+    const json = JSON.stringify(cats);
+    console.log('window unload, json is', json);
+    window.localStorage.setItem('cats', json);
+});
+
+window.resetData = () => {
+    window.localStorage.removeItem('cats');
+    cats = undefined;
+    window.location.reload();
+};
