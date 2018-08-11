28d1cb0c55ac9cf9096c724b095c898a8e8098cc
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-11 08:07:44 -0700

    setting up footers

--- js/components/AppResults.js
+++ js/components/AppResults.js
@@ -1,9 +1,12 @@
 import html from '../html.js';
 import Header from './Header.js';
+import Footer from './Footer.js';
 
 let template = function() {
     return html`
         <header></header>
+        <main></main>
+        <footer></footer>
    `;
 };
 
@@ -15,10 +18,13 @@ export default class AppResults {
     render() {
         let dom = template();
         this.header = dom.querySelector('header');
+        this.footer = dom.querySelector('footer');
 
         const header = new Header();
+        const footer = new Footer();
 
-        this.header.appendChild(header.render());
+        this.header.appendChild(header.render());        
+        this.footer.appendChild(footer.render());
 
         return dom;
     }
